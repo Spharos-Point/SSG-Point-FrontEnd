545360743b5fae3aad0007f7842fb286bea159bf
dow-dad <sojunyeong301@gmail.com>
2023-09-06 21:16:40 +0900

    마이페이지 포인트 표시 페이지 제작

--- app/mypoint/chgPntPwdCert/page.tsx
+++ app/mypoint/chgPntPwdCert/page.tsx
@@ -1,14 +1,16 @@
 import JoinAuth from '@/components/page/auth/Auth'
-import FindIdBottom from '@/components/page/member/FindIdBottom'
 import ChgPntPwdTop from '@/components/page/mypoint/chgPntPwdCert/ChgPntPwdTop'
+import Note from '@/components/page/note/note'
+import { signupNotice } from '@/data/noticeData'
 import React from 'react'
+
 
 export default function ChgPntPwdCert() {
   return (
     <section>
         <ChgPntPwdTop />
         <JoinAuth/>
-        <FindIdBottom/>
+        <Note note={signupNotice}/>
     </section>
   )
 }
