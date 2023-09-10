f7e8e5db209b8f307388b7f6baaa9472cdd3bc39
dow-dad <sojunyeong301@gmail.com>
2023-09-10 20:26:25 +0900

    포인트 선물하기 페이지 선물 메시지 선택 버튼 기능 및 폼 구현

--- components/page/mypoint/pntGiftMainSub/PntGiftMessageForm.tsx
+++ components/page/mypoint/pntGiftMainSub/PntGiftMessageForm.tsx
@@ -1,12 +1,16 @@
-'use client'
-import React, { useState } from 'react'
-import styles from './PntGiftMainSub.module.css'
+import React, { useState } from 'react';
+import styles from './PntGiftMainSub.module.css';
+import GiftmessageFormSelector from './GiftmessageFormSelector';
 
 export default function PntGiftMessageForm() {
+    //선물 메시지 사용 여부
     const [isMesageUseChecked, setIsMesageUseChecked] = useState(false);
     const [isMesageNoChecked, setIsMesageNoChecked] = useState(false);
-    
-    // 둘 중 하나만 체크되도록 하는 함수
+
+    //선물 메시지 선택
+    const [selectedCard, setSelectedCard] = useState(1);
+
+    // 사용여부 둘 중 하나만 체크
     const handleIsMesageUseChecked = () => {
         setIsMesageUseChecked(true);
         setIsMesageNoChecked(false);
@@ -15,38 +19,55 @@ export default function PntGiftMessageForm() {
         setIsMesageUseChecked(false);
         setIsMesageNoChecked(true);
     };
+
+    const handleButtonClick = (value: React.SetStateAction<number>) => {
+        setSelectedCard(value); // 버튼 클릭에 따라 selectedCard 업데이트
+    };
+
     return (
         <div className='px-5'>
             <p className='text-[12px] leading-[19px] mt-5'>포인트 선물 메시지</p>
-                <div className={styles.Message_checkBox}>
-                    <input
-                        type="checkbox"
-                        name="isMesageUse"
-                        id="isMesageUse"
-                        checked={isMesageUseChecked}
-                        onChange={handleIsMesageUseChecked}
-                    />
-                    <label htmlFor="isMesageUse">사용</label>
+            <div className={styles.Message_checkBox}>
+                <input
+                    type="checkbox"
+                    name="isMesageUse"
+                    id="isMesageUse"
+                    checked={isMesageUseChecked}
+                    onChange={handleIsMesageUseChecked}
+                />
+                <label htmlFor="isMesageUse">사용</label>
 
-                    <input
-                        type="checkbox"
-                        name="isMasageNo"
-                        id="isMasageNo"
-                        checked={isMesageNoChecked}
-                        onChange={handleIsMesageNoChecked}
-                    />
-                    <label htmlFor="isMasageUNo">사용 안함</label>
-                </div>
+                <input
+                    type="checkbox"
+                    name="isMasageNo"
+                    id="isMasageNo"
+                    checked={isMesageNoChecked}
+                    onChange={handleIsMesageNoChecked}
+                />
+                <label htmlFor="isMasageUNo">사용 안함</label>
+            </div>
 
             {/* isMesageNoChecked 유무에 따라 표시 or null */}
             {isMesageUseChecked ? (
                 <>
                     <div className='box-border my-5'>
-                        <div className={styles.gift_card_paper}>
+                        {/* 메시지 버튼에 따라 메시지 폼 선택하여 css 적용 */}
+                        <GiftmessageFormSelector selectedCard={selectedCard} handleButtonClick={handleButtonClick} />
+                        <div className={`${selectedCard === 1 ? styles.gift_card_paper_01
+                                        : selectedCard === 2 ? styles.gift_card_paper_02
+                                        : selectedCard === 3 ? styles.gift_card_paper_03
+                                        : selectedCard === 4 ? styles.gift_card_paper_04
+                                        : ''
+                        }`}>
                             <div className='w-[315px] mx-auto'>
                                 <div className={styles.card_cotents}>
-                                    <textarea name="cardContents" id="메세지카드내용" maxLength={50} rows={3}
-                                        className={styles.card_cotents_textarea} />
+                                    <textarea
+                                        name="cardContents"
+                                        id="메세지카드내용"
+                                        maxLength={50}
+                                        rows={3}
+                                        className={styles.card_cotents_textarea}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -61,7 +82,6 @@ export default function PntGiftMessageForm() {
                     </div>
                 </>
             ) : null}
-
         </div>
-    )
+    );
 }
