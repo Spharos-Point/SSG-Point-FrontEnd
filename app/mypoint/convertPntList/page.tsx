726236466395a36de0a3f9ab311d985fa97547cc
dow-dad <sojunyeong301@gmail.com>
2023-09-15 16:07:37 +0900

    오타수정

--- app/mypoint/convertPntList/page.tsx
+++ app/mypoint/convertPntList/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import styles from './ConvertPntList.module.css'
+import styles from './convertPntList.module.css'
 import ConvertPntURL from '@/components/page/mypoint/convertPntList/convertPntURL'
 
 export default function convertPntList() {
